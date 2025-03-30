160afdbb2abce4b9a2ae37d137a9f00c88b22376
dayo777 <oruns@outlook.com>
2025-03-30 21:41:26 +0100

    added the access & secret key to the required providers

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -3,6 +3,8 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "5.93.0"
+      aws_access_key = var.aws_access_key
+      aws_secret_key = var.aws_secret_key
     }
   }
 
@@ -19,4 +21,5 @@ terraform {
 provider "aws" {
   # Configuration options
   region = var.main_region
+
 }
